6c45f6546db3668ca40dc2740134c14074b1c42a
Lars Mühmel <lars@muehml.eu>
2025-09-25 00:13:45 +0200

    Configure minecraft settings

--- minecraft.nix
+++ minecraft.nix
@@ -9,6 +9,11 @@
     whitelist = {
       "BurNiinTRee" = "c01f2fec-a3c4-44a4-ba88-2ea59e3184b0";
     };
+    serverProperties = {
+      white-list = true;
+      difficulty = "normal";
+      motd = "Isterbandet's Minecraft Server";
+    };
   };
 
   persist.directories = [ config.services.minecraft-server.dataDir ];
