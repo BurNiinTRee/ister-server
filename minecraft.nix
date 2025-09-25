78eb6776a9859b2f2aa9c5650aa09a4634730d64
Lars Mühmel <lars@muehml.eu>
2025-09-25 21:01:03 +0200

    Allowlist Nils

--- minecraft.nix
+++ minecraft.nix
@@ -9,6 +9,7 @@
     whitelist = {
       "BurNiinTRee" = "c01f2fec-a3c4-44a4-ba88-2ea59e3184b0";
       "nagisalaw" = "8f49e82c-d776-4a13-8880-510e1128df23";
+      "John_Cheese" = "9538b89e-d589-402b-988e-d57a9cc7ca14";
     };
     serverProperties = {
       white-list = true;
