dfed91cc63a6efd047b0c87e225914d2261f6043
Lars Mühmel <lars@muehml.eu>
2025-09-25 20:43:41 +0200

    allowlist Anna

--- minecraft.nix
+++ minecraft.nix
@@ -8,6 +8,7 @@
     openFirewall = true;
     whitelist = {
       "BurNiinTRee" = "c01f2fec-a3c4-44a4-ba88-2ea59e3184b0";
+      "nagisalaw" = "8f49e82c-d776-4a13-8880-510e1128df23";
     };
     serverProperties = {
       white-list = true;
