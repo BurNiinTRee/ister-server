044bc9e16dec41ecbdcf2810f38ae774cd78a6a5
Lars Mühmel <lars@muehml.eu>
2025-09-28 11:42:09 +0200

    make server config imperative
    mainly to make white-listing people easier

--- minecraft.nix
+++ minecraft.nix
@@ -4,19 +4,8 @@
   services.minecraft-server = {
     enable = true;
     eula = true;
-    declarative = true;
+    declarative = false;
     openFirewall = true;
-    whitelist = {
-      "BurNiinTRee" = "c01f2fec-a3c4-44a4-ba88-2ea59e3184b0";
-      "nagisalaw" = "8f49e82c-d776-4a13-8880-510e1128df23";
-      "John_Cheese" = "9538b89e-d589-402b-988e-d57a9cc7ca14";
-      "petpeng" = "6363cf85-ab7c-45d3-81f4-63a9d1037c92";
-    };
-    serverProperties = {
-      white-list = true;
-      difficulty = "normal";
-      motd = "Isterbandet's Minecraft Server";
-    };
   };
 
   persist.directories = [ config.services.minecraft-server.dataDir ];
