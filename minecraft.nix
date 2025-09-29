de7618e4717fcbabf7673e1afa9e81caaad8693d
Lars Mühmel <lars@muehml.eu>
2025-09-29 20:06:24 +0200

    Install Create

--- minecraft.nix
+++ minecraft.nix
@@ -1,12 +1,21 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   nixpkgs.config.allowUnfree = true;
-  services.minecraft-server = {
+  services.minecraft-servers = {
     enable = true;
     eula = true;
-    declarative = false;
     openFirewall = true;
+    servers = {
+      create = {
+        enable = true;
+        package = pkgs.neoforge-servers_1_21_1;
+        symlinks = {
+          config = ./JustCreateSmp/config;
+          mods = ./JustCreateSmp/mods;
+        };
+      };
+    };
   };
 
-  persist.directories = [ config.services.minecraft-server.dataDir ];
+  persist.directories = [ config.services.minecraft-server.dataDir config.services.minecraft-servers.dataDir ];
 }
